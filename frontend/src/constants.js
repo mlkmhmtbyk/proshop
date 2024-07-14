8e7b970d5e2807c8ec1942312fee414b9f011f72
mlkmhmtbyk <biyik.melikmehmet@gmail.com>
2024-07-15 01:14:23 +0300

    authentication bitirildi

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,6 +2,7 @@
 //  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
 
 export const BASE_URL = "";
+
 export const PRODUCTS_URL = "/api/products";
 
 export const USERS_URL = "/api/users";
